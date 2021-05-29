e3250be64f44b9b0a1098c4ac15f536ca75a4458
Sanjana-11 <sanjanakalathil@gmail.com>
2021-05-29 12:22:39 +0530

    added gender choices

--- myapp/models.py
+++ myapp/models.py
@@ -1,20 +1,30 @@
 from django.db import models
 from django.db.models.fields import CharField
-from django.db.models.fields import DateField
-from django.db.models.fields import TimeField
+
 
 
 # Create your models here.
 
 class event(models.Model):
+
+    gender_choices=(
+        ('M','male'),
+        ('F', 'female'),
+        ('N-B','non-binary'),
+        ('N/A','prefer not to say')
+    )
+
+
     title = models.CharField(max_length=50,null=False)
     date = models.DateField(null=True,blank=True)
     time = models.TimeField(null=True,blank=True)
+    gender = models.CharField(max_length=11,choices=gender_choices,null=True,blank=True)
     description = models.CharField(max_length=150,null=False)
     
     def __str__(self):
         return self.title
 
+     
 
 
     
